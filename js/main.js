9bbaa872975a040ced812efbc489ec77a59a0c9e
enfllome <enfllome.98@mail.ru>
2021-01-31 14:19:13 +0300

    Изм.функц.рандома чисел на стрелочную,переим.аргум

--- js/main.js
+++ js/main.js
@@ -1,16 +1,18 @@
+'use strict';
+
 const firstRangeNumber = -5;
 const secondRangeNumber = 6;
 
-const getRandomInt = function (a, b) {
-  const min = Math.ceil(Math.min(a, b));
-  const max = Math.floor(Math.max(a, b));
+const getRandomInt = (bottom, top) => {
+  const min = Math.ceil(Math.min(bottom, top));
+  const max = Math.floor(Math.max(bottom, top));
   const result = Math.abs(Math.floor(Math.random() * (max - min + 1)) + min); //Максимум и минимум включаются
   return result;
 }
 
-const getRandomFloat = function (a, b, roundUp) {
-  const min = Math.ceil(Math.min(a, b));
-  const max = Math.floor(Math.max(a, b));
+const getRandomFloat = (bottom, top, roundUp) => {
+  const min = Math.ceil(Math.min(bottom, top));
+  const max = Math.floor(Math.max(bottom, top));
   const result = Math.abs(Math.random() * (max - min) + min); //Максимум и минимум включаются
   return result.toFixed(roundUp);
 }
