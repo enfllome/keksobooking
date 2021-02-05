8a0c9e6831b2826cb8508f4fe702b7367cfb3236
enfllome <enfllome.98@mail.ru>
2021-02-05 17:39:58 +0300

    пофиксил функцию getRandomFloat

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 'use strict';
 
-const firstRangeNumber = -5;
-const secondRangeNumber = 6;
+const firstRangeNumber = 139.70000;
+const secondRangeNumber = 139.80000;
 
 const getRandomInt = (bottom, top) => {
   const min = Math.ceil(Math.min(bottom, top));
@@ -11,15 +11,14 @@ const getRandomInt = (bottom, top) => {
 }
 
 const getRandomFloat = (bottom, top, roundUp = 2) => {
-  const min = Math.ceil(Math.min(bottom, top));
-  const max = Math.floor(Math.max(bottom, top));
+  const min = Math.min(bottom, top);
+  const max = Math.max(bottom, top);
   const result = Math.abs(Math.random() * (max - min) + min); //Максимум и минимум включаются
   return result.toFixed(roundUp);
 }
 
 const intResult = getRandomInt(firstRangeNumber, secondRangeNumber);
-const floatResult = getRandomFloat(firstRangeNumber, secondRangeNumber, 3);
+const floatResult = getRandomFloat(firstRangeNumber, secondRangeNumber, 2);
 
 alert('Рандомное целое число: ' + intResult);
 alert('Рандомное дробное число: ' + floatResult);
-
