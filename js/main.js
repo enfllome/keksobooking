ea41c7c757ee804cf789a34ab00c611e08c06ccf
enfllome <enfllome.98@mail.ru>
2021-02-14 17:41:15 +0300

    Сгенерировал временные данные

--- js/main.js
+++ js/main.js
@@ -1,48 +1,33 @@
 'use strict';
 
-
-const getRandomInt = (bottom, top) => {
-  const min = Math.ceil(Math.min(bottom, top));
-  const max = Math.floor(Math.max(bottom, top));
-  const result = Math.abs(Math.floor(Math.random() * (max - min + 1)) + min); //Максимум и минимум включаются
-  return result;
-}
-
-const getRandomFloat = (bottom, top, roundUp = 2) => {
-  const min = Math.min(bottom, top);
-  const max = Math.max(bottom, top);
-  const result = Math.abs(Math.random() * (max - min) + min); //Максимум и минимум включаются
-  return result.toFixed(roundUp);
-}
-
-const offerTitles = [
+const OFFER_TITLES = [
   'Лучшее предложение',
   'Эконом вариант',
   'Для любителей старины',
 ];
 
-const priceList = [100, 200, 300, 400];
+const PRICE_LIST = [100, 200, 300, 400];
 
-const housingType = [
+const HOUSING_TYPE = [
   'palace',
   'flat',
   'house',
   'bungalow',
 ]
 
-const chekinTimes = [
+const CHEK_IN_TIMES = [
   '12::00',
   '13::00',
   '14::00',
 ];
 
-const chekOutTimes = [
+const CHECK_OUT_TIMES = [
   '12::00',
   '13::00',
   '14::00',
 ];
 
-const housingFeatures = [
+const HOUSING_FEATURES = [
   'wifi',
   'dishwasher',
   'parking',
@@ -51,41 +36,80 @@ const housingFeatures = [
   'conditioner',
 ];
 
-const photos = [
+const DESCRIPTIONS = [
+  'Уютная квартира для семейной пары в центре Москвы.',
+  'Солнечная квартика в Санкт-Петербурге',
+  'Чистая и свежая комната в Уфе',
+];
+
+const PHOTOS = [
   'http://o0.github.io/assets/images/tokyo/hotel1.jpg',
   'http://o0.github.io/assets/images/tokyo/hotel2.jpg',
   'http://o0.github.io/assets/images/tokyo/hotel3.jpg',
 ];
 
-const OFFERS_COUNT = 10;
+const OFFERS_COUNT = 3;
+
+const getRandomIntNumber = (bottom, top) => {
+  const min = Math.ceil(Math.min(bottom, top));
+  const max = Math.floor(Math.max(bottom, top));
+  const result = Math.abs(Math.floor(Math.random() * (max - min + 1)) + min); //Максимум и минимум включаются
+  return result;
+}
+
+const getRandomFloatNumber = (bottom, top, roundUp = 2) => {
+  const min = Math.min(bottom, top);
+  const max = Math.max(bottom, top);
+  const result = Math.abs(Math.random() * (max - min) + min); //Максимум и минимум включаются
+  return result.toFixed(roundUp);
+}
 
 const getRandomArrayElement = (elements) => {
-  return elements[getRandomInt(0, elements.length - 1)];
+  return elements[getRandomIntNumber(0, elements.length - 1)];
 };
 
-const createRandomOffer = () => {
+const getRandomCoordinate = () => {
   return {
-    author: {
-      avatar: `img/avatars/user0${getRandomInt(1, 8)}.png`,
-    },
-    offer: {
-      title: getRandomArrayElement(offerTitles),
-      address: '{{location.x}}, {{location.y}}',
-      price: getRandomArrayElement(priceList),
-      type:  getRandomArrayElement(housingType),
-      rooms: 1,
-      guests: 2,
-      checkin: getRandomArrayElement(chekinTimes),
-      checkout: getRandomArrayElement(chekOutTimes),
-      features: getRandomArrayElement(housingFeatures),
-      description: 'Уютная квартира для семейной пары в центре Москвы.',
-      photos: getRandomArrayElement(photos),
-    },
-    location: {
-      x: getRandomFloat(35.65000, 35.70000),
-      y: getRandomFloat(139.70000, 139.80000),
-    },
+    x: getRandomFloatNumber(35.65000, 35.70000),
+    y: getRandomFloatNumber(139.70000, 139.80000),
   }
 }
 
-const offers = new Array(OFFERS_COUNT).fill(null).map(() => createRandomOffer());
+const getNoRepeatElements = (elements) => {
+  return elements.filter(() => Math.random() > 0.5);
+}
+
+const photosList = new Array(getRandomIntNumber(1, PHOTOS.length))
+  .fill(null)
+  .map(() => getRandomArrayElement(PHOTOS));
+
+const createRandomOffer = () => {
+
+  const coordinates = getRandomCoordinate();
+
+  return {
+    author: {
+      avatar: `img/avatars/user0${getRandomIntNumber(1, 8)}.png`,
+    },
+    offer: {
+      title: getRandomArrayElement(OFFER_TITLES),
+      address: coordinates,
+      price: getRandomArrayElement(PRICE_LIST),
+      type:  getRandomArrayElement(HOUSING_TYPE),
+      rooms: getRandomIntNumber(1, 5),
+      guests: getRandomIntNumber(1, 3),
+      checkin: getRandomArrayElement(CHEK_IN_TIMES),
+      checkout: getRandomArrayElement(CHECK_OUT_TIMES),
+      features: getNoRepeatElements(HOUSING_FEATURES),
+      description: getRandomArrayElement(DESCRIPTIONS),
+      photos: photosList,
+    },
+    location: coordinates,
+  }
+}
+
+const offers = new Array(OFFERS_COUNT)
+  .fill(null)
+  .map(() => createRandomOffer());
+
+alert(offers);
